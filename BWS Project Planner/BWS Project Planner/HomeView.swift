288c2ca6971a18b469419014f0b31d404660a525
Spencer Blunt <sblunt3@csu.fullerton.edu>
2026-04-21 12:14:04 -0700

    addition

--- BWS Project Planner/BWS Project Planner/HomeView.swift
+++ BWS Project Planner/BWS Project Planner/HomeView.swift
@@ -1,11 +1,23 @@
-
-
-
-
-
+//
+//  ContentView.swift
+//  BWS Project Planner
+//
+//  Created by Spencer Blunt on 4/18/26.
+//
 
 
 import SwiftUI
 import SwiftData
 
+
+struct HomeView: View {
+    
+    var body: some View {
+        
+        ZStack {
+            
+            
+        }
+    }
+}
 

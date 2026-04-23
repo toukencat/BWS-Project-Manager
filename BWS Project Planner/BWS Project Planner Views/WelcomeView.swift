edf63fb0f2874baf00170dafd40a2216a76b48f0
Spencer Blunt <sblunt3@csu.fullerton.edu>
2026-04-22 19:37:07 -0700

    adding home page and welcome page

---
+++ BWS Project Planner/BWS Project Planner Views/WelcomeView.swift
@@ -0,0 +1,18 @@
+//
+//  WelcomeView.swift
+//  BWS Project Planner
+//
+//  Created by Spencer Blunt on 4/22/26.
+//
+
+import SwiftUI
+
+struct WelcomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WelcomeView()
+}
